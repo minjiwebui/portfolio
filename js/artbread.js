27e816557501e1a25ce52f61e25b790e7db300a9
minjiwebui <minamall376@gmail.com>
2023-02-24 06:56:31 +0000

    text shadow 활용

--- js/artbread.js
+++ js/artbread.js
@@ -1,3 +1,7 @@
+// top 서브메뉴(커뮤니티)
+$(".submenu").hover(function () {
+    $(".subgnb").toggleClass('on');
+});
 // sec01 슬라이드
 var swiper1 = new Swiper(".basic01", {
     spaceBetween: 30,
@@ -23,10 +27,6 @@ $(".newsL h3 span span").click(function () {
     var tabindex = $(this).index();
     $(this).addClass('on').siblings().removeClass('on');
     $(".newsL ul").eq(tabindex).addClass('on').siblings().removeClass('on');
-});
-// top 서브메뉴(커뮤니티)
-$(".submenu").hover(function () {
-    $(".subgnb").toggleClass('on');
 });
 // header 모바일 서브메뉴
 $(".header2 .inner > i").click(function () {
