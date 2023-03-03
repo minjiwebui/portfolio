4f0a696ad92e3e26572d7053c573ea4297123ed4
minjiwebui <minamall376@gmail.com>
2023-03-03 08:26:03 +0000

    피니쉬

--- js/artbread_sub.js
+++ js/artbread_sub.js
@@ -9,14 +9,18 @@ $(".search > i").click(function() {
 $(".popup .fa-times").click(function() {
     $(".popup").removeClass("on");
 });
-
-
-
-
-
-
-
-
+// sec01 이미지 탭
+$(".sec01 .leftbox div").click(function(){
+    var tabindex = $(this).index();
+    $(this).addClass('on').siblings().removeClass('on');
+    $(".sec01 .rightbox div").eq(tabindex).addClass('on').siblings().removeClass('on');
+});
+$(".sec01 .leftbox div").eq(0).trigger("click")
+$(".sec01 .leftbox div").hover(function(){
+    var tabindex = $(this).index();
+    $(this).addClass('on').siblings().removeClass('on');
+    $(".sec01 .rightbox div").eq(tabindex).addClass('on').siblings().removeClass('on');
+});
 // header 모바일 검색팝업창
 $(".header2 .inner > .fa-search").click(function() {
     $(".header2 .popup").addClass("on");
