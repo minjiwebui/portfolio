27818549c3452a5b6b69a6a7c40c295a017bac5f
minjiwebui <minamall376@gmail.com>
2023-03-09 03:43:07 +0000

    고정 버튼 스크롤 이벤트 완료!

--- js/artbread.js
+++ js/artbread.js
@@ -1,8 +1,8 @@
-// top 서브메뉴(커뮤니티)
+// top 서브메뉴(커뮤니티)/(공통요소)
 $(".submenu").hover(function () {
     $(".subgnb").toggleClass('on');
 });
-// top 검색팝업창
+// top 검색팝업창(공통요소)
 $(".search > i").click(function() {
     $(".popup").addClass("on");
 });
@@ -47,14 +47,14 @@ $(".newsL h3 span span").click(function () {
     $(this).addClass('on').siblings().removeClass('on');
     $(".newsL ul").eq(tabindex).addClass('on').siblings().removeClass('on');
 });
-// header 모바일 검색팝업창
+// header 모바일 검색팝업창(공통요소)
 $(".header2 .inner > .fa-search").click(function() {
     $(".header2 .popup").addClass("on");
 });
 $(".header2 .popup .fa-times").click(function() {
     $(".header2 .popup").removeClass("on");
 });
-// header 모바일 서브메뉴
+// header 모바일 서브메뉴(공통요소)
 $(".header2 .inner > .fa-bars").click(function () {
     $(".gnbmb").toggleClass("on");
     $(this).toggleClass("fa-times");
--- js/artbread_sub.js
+++ js/artbread_sub.js
@@ -1,8 +1,8 @@
-// top 서브메뉴(커뮤니티)
+// top 서브메뉴(커뮤니티)/(공통요소)
 $(".submenu").hover(function () {
     $(".subgnb").toggleClass('on');
 });
-// top 검색팝업창
+// top 검색팝업창(공통요소)
 $(".search > i").click(function() {
     $(".popup").addClass("on");
 });
@@ -35,22 +35,23 @@ var swiper = new Swiper(".basic02", {
     },
     loop: true
   });
-
-
-
-
-
-
-
-
-// header 모바일 검색팝업창
+// 태블릿, 모바일 하단 고정 버튼 스크롤
+$(document).scroll(function() {
+    var scrolltop = $(window).scrollTop();
+    if(scrolltop > 465 && scrolltop < 1413){
+        $(".fixebt").addClass("off")
+    } else{
+        $(".fixebt").removeClass("off")
+    }
+});
+// header 모바일 검색팝업창(공통요소)
 $(".header2 .inner > .fa-search").click(function() {
     $(".header2 .popup").addClass("on");
 });
 $(".header2 .popup .fa-times").click(function() {
     $(".header2 .popup").removeClass("on");
 });
-// header 모바일 서브메뉴
+// header 모바일 서브메뉴(공통요소)
 $(".header2 .inner > .fa-bars").click(function () {
     $(".gnbmb").toggleClass("on");
     $(this).toggleClass("fa-times");
