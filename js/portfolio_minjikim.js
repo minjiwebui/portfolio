d01fdf20c61990379e747f3112251f406137f454
minjiwebui <minamall376@gmail.com>
2023-03-28 08:15:38 +0000

    z

---
+++ js/portfolio_minjikim.js
@@ -0,0 +1,11 @@
+// sec04 슬라이드
+var swiper1 = new Swiper(".basic01", {
+    centeredSlides: true,
+    effect: "fade",
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    slidesPerView: 1,
+});
