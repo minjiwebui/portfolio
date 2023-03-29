0c14430d7f19d98fd52b5cba7d3ee5ca1ad25d31
minjiwebui <minamall376@gmail.com>
2023-03-29 02:01:14 +0000

    구분선 제거

--- js/portfolio_minjikim.js
+++ js/portfolio_minjikim.js
@@ -9,3 +9,14 @@ var swiper1 = new Swiper(".basic01", {
     // },
     slidesPerView: 1,
 });
+// 스크롤 버튼
+$("header .nav li").each(function() {
+    var thisOffset = $("." + $(this).data('class')).offset().top;
+    console.log(thisOffset)
+    $(this).click(function() {
+        $("html, body").animate({
+            scrollTop: thisOffset -105
+        }, 1000);
+        $(this).addClass('on').siblings().removeClass("on");
+    });
+});
